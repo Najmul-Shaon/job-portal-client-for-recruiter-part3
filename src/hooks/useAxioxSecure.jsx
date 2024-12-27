8679232da65d034dbe89470606999ccbcf425f9b
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-12-27 23:19:32 +0600

    used interceptors

--- src/hooks/useAxioxSecure.jsx
+++ src/hooks/useAxioxSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
@@ -6,6 +9,31 @@ const axiosInstance = axios.create({
 });
 
 const useAxioxSecure = () => {
+  const { signOutUser } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error in interceptor", error);
+
+        if (error.status === 401 || error.status === 403) {
+          console.log("logged out");
+          signOutUser()
+            .then(() => {
+              console.log("logged out user");
+              navigate("/signIn");
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
   return axiosInstance;
 };
 
